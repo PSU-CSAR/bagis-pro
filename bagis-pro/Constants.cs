11a51d64a896f337c38e635f152850821738737e
lbross <lbross@pdx.edu>
2024-03-04 11:33:23 -0800

    issue #50: Add MTBS burn severity data source

--- bagis-pro/Constants.cs
+++ bagis-pro/Constants.cs
@@ -325,6 +325,7 @@ namespace bagis_pro
         public const string DATA_SOURCES_ALASKA = "Alaska Data Sources";
         public const string DATA_TYPE_FIRE_HISTORY = "Fire History";
         public const string DATA_TYPE_FIRE_CURRENT = "Fire Current";
+        public const string DATA_TYPE_FIRE_BURN_SEVERITY = "Fire Burn Severity";
 
         public const int VALUE_NO_DATA_9999 = -9999;
         public const string VALUE_NO_DATA = "NoData";
