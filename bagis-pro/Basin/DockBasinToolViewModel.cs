d6eb6917aa72dc139f60b1ec6cc968c5b4778f38
lbross <lbross@pdx.edu>
2025-02-12 11:58:04 -0800

    issue #59: Start laying out Basin Tool UI

---
+++ bagis-pro/Basin/DockBasinToolViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bagis_pro.Basin
+{
+    internal class DockBasinToolViewModel : DockPane
+    {
+        private const string _dockPaneID = "bagis_pro_Basin_DockBasinTool";
+
+        protected DockBasinToolViewModel() { }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class DockBasinTool_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            DockBasinToolViewModel.Show();
+        }
+    }
+}
