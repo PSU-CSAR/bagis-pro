6c5703a321242343f9deb413884a2a603e7c3e69
lbross <lbross@pdx.edu>
2025-04-01 11:17:32 -0700

    issue #59: Configure icons for toolbars

--- bagis-pro/Basin/DockBasinToolViewModel.cs
+++ bagis-pro/Basin/DockBasinToolViewModel.cs
@@ -385,6 +385,11 @@ namespace bagis_pro.Basin
                     // These are gp environment variables to be used for gp tools in this function; There is no way to set this at the session/project level
                     var env = Geoprocessing.MakeEnvironmentArray(workspace: ParentFolder);
 
+                    if (bNeedToClipDem)
+                    {
+
+                    }
+
                 });
             }
         }
